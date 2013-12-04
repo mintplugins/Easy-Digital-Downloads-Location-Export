5a53ba8e8aa213ebb0ac792b7cb3c6e2a51b7eac
Phil Johnston <johnstonphilip@gmail.com>
2013-12-04 15:52:50 -0500

    Fix class bug
    include class export
    
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/misc-functions/export-functions.php
+++ includes/misc-functions/export-functions.php
@@ -1,5 +1,22 @@
 <?php
 /**
+ * Exports Functions
+ *
+ * These are functions are used for exporting data from Easy Digital Downloads - Locaton Export
+ *
+ * @package     EDD
+ * @subpackage  Admin/Export
+ * @copyright   Copyright (c) 2013, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+require_once EDD_PLUGIN_DIR . 'includes/admin/reporting/class-export.php';
+
+/**
  * Exports all the payments stored in Payment History to a CSV file using the
  * EDD_Export class.
  *
@@ -7,7 +24,7 @@
  * @return void
  */
 function edd_export_payment_history_by_location() {
-	
+		
 	$payments_export = new EDD_Payments_By_Location_Export();
 
 	$payments_export->export();
